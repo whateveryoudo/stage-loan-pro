590086115fb9c3a6370adf2c334c4d7d3dd35fd9
youkaixing <1795261900@qq.com>
2021-05-07 09:36:59 +0800

    修改接口地址请求

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,8 @@
  * @Description: 配置编译环境和线上环境之间的切换
  * @Autor: ykx
  * @Date: 2021-05-06 23:10:38
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-05-07 00:47:00
+ * @LastEditors: your name
+ * @LastEditTime: 2021-05-07 09:36:40
  */
 let baseUrl;
 let routerMode = 'history';
@@ -12,8 +12,9 @@ let imgBaseUrl1 = 'https://gw.alicdn.com/i3/';
 if (process.env.NODE_ENV == 'development') {
   baseUrl = 'http://localhost:5050';
 } else if (process.env.NODE_ENV == 'production') {
-  baseUrl = 'https://quickloanapi.applinzi.com';
+  baseUrl = 'http://quickloanapi.applinzi.com';
 }
+// baseUrl = 'http://quickloanapi.applinzi.com';
 export {
   baseUrl,
   routerMode,
