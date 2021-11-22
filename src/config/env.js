c8f3be7d543e1c9e9640d352a80ef2359efeef53
游开兴 <1795261900@qq.com>
2021-11-22 15:43:17 +0800

    feat:添加本地虚拟验证码

--- src/config/env.js
+++ src/config/env.js
@@ -3,21 +3,17 @@
  * @Autor: ykx
  * @Date: 2021-05-06 23:10:38
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-10-27 00:05:35
+ * @LastEditTime: 2021-11-22 15:43:02
  */
-let baseUrl;
-let routerMode = 'history';
-let imgBaseUrl = 'https://gw.alicdn.com/tfs/';
-let imgBaseUrl1 = 'https://gw.alicdn.com/i3/';
+let baseUrl
+let routerMode = 'history'
+let imgBaseUrl = 'https://gw.alicdn.com/tfs/'
+let imgBaseUrl1 = 'https://gw.alicdn.com/i3/'
 if (process.env.NODE_ENV == 'development') {
-  baseUrl = 'http://localhost:5050';
+  // 这里为非@记录值，添加前缀loan
+  baseUrl = 'http://localhost:5050'
 } else if (process.env.NODE_ENV == 'production') {
   // baseUrl = 'https://quickloanapi.applinzi.com';
-  baseUrl = 'http://cdnci.com';
+  baseUrl = 'http://loan.dlzjs.cn'
 }
-export {
-  baseUrl,
-  routerMode,
-  imgBaseUrl,
-  imgBaseUrl1,
-}
+export { baseUrl, routerMode, imgBaseUrl, imgBaseUrl1 }
