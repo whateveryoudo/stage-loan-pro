0d5e0a0186af7ddf926008a9ae933e2612fbb03f
youkaixing <15982819091@163.com>
2022-02-21 10:58:16 +0800

    feat:修改请求地址

--- src/config/env.js
+++ src/config/env.js
@@ -3,18 +3,17 @@
  * @Autor: ykx
  * @Date: 2021-05-06 23:10:38
  * @LastEditors: your name
- * @LastEditTime: 2021-11-23 10:26:02
+ * @LastEditTime: 2022-02-21 10:57:57
  */
 let baseUrl
 let routerMode = 'history'
 let imgBaseUrl = 'https://gw.alicdn.com/tfs/'
 let imgBaseUrl1 = 'https://gw.alicdn.com/i3/'
 if (process.env.NODE_ENV == 'development') {
-  // 这里为非@记录值，添加前缀www
-  // baseUrl = 'http://localhost:5050'
-  baseUrl = 'http://www.qdqkon.cn'
+  // 这里为非@记录值，添加前缀loan
+  baseUrl = 'http://localhost:5050'
 } else if (process.env.NODE_ENV == 'production') {
   // baseUrl = 'https://quickloanapi.applinzi.com';
-  baseUrl = 'http://www.qdqkon.cn'
+  baseUrl = 'http://loan.dlzjs.cn'
 }
 export { baseUrl, routerMode, imgBaseUrl, imgBaseUrl1 }
