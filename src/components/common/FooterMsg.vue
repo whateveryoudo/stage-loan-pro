085d5c38d60069af6468be1b409c5307cf61a4f8
youkaixing <15982819091@163.com>
2022-09-27 15:35:35 +0800

    feat:修改底部备注

--- src/components/common/FooterMsg.vue
+++ src/components/common/FooterMsg.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: ykx
  * @Date: 2021-05-13 15:25:39
- * @LastEditTime: 2022-08-01 17:25:17
+ * @LastEditTime: 2022-09-27 15:28:13
  * @LastEditors: your name
  * @Description: 底部备注信息
  * @FilePath: \stage-load-pro\src\components\common\FooterMsg.vue
@@ -11,7 +11,8 @@
     <p>贷款有风险，借款需谨慎；请根据个人能力合理贷款，理性消费，避免逾期</p>
     <p>贷款额度、放款时间以实际审批结果为准</p>
     <p>贷款资金由重庆市黑卡小额贷款有限公司提供</p>
-    <p>重庆市黑卡小额贷款有限公司 渝ICP备20000928号-1</p>
+    <!-- <p>重庆市黑卡小额贷款有限公司 渝ICP备20000928号-1</p> -->
+    <p>成都云创安信息技术咨询有限公司 <a href="https://beian.miit.gov.cn/#/Integrated/index">蜀ICP备2021013139号</a></p>
   </div>
 </template>
 
