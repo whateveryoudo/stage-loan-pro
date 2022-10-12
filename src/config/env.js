2900f212348bf47bd3e044741731df3e825e1fda
游开兴 <15982819091@163.com>
2022-10-12 12:51:39 +0800

    feat:请求地址修改

--- src/config/env.js
+++ src/config/env.js
@@ -2,19 +2,21 @@
  * @Description: 配置编译环境和线上环境之间的切换
  * @Autor: ykx
  * @Date: 2021-05-06 23:10:38
- * @LastEditors: your name
- * @LastEditTime: 2022-06-24 16:02:42
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-10-12 12:01:35
  */
 let baseUrl
 let routerMode = 'history'
 let imgBaseUrl = 'https://gw.alicdn.com/tfs/'
 let imgBaseUrl1 = 'https://gw.alicdn.com/i3/'
 if (process.env.NODE_ENV == 'development') {
-  // 这里为非@记录值，添加前缀www
   // baseUrl = 'http://localhost:5050'
-  baseUrl = 'http://www.qdqkon.cn'
+  // baseUrl = 'http://www.qdqfa.cn'
+  baseUrl = 'http://loan.dlbgp.cn'
+
 } else if (process.env.NODE_ENV == 'production') {
   // baseUrl = 'https://quickloanapi.applinzi.com';
-  baseUrl = 'http://www.qdqkon.cn'
+  // baseUrl = 'http://www.qdqfa.cn'
+  baseUrl = 'http://loan.dlbgp.cn'
 }
 export { baseUrl, routerMode, imgBaseUrl, imgBaseUrl1 }
